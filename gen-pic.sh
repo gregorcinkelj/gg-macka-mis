60b4eff44c36e720e101ad365c2b0120da7d906f
Justin Cinkelj <justin.cinkelj@xlab.si>
2018-01-27 14:40:02 +0100

    add pictures
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

---
+++ gen-pic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://www.eastcottvets.co.uk/uploads/Animals/gingerkitten.jpg
+convert pic/gingerkitten-orig.jpg -trim -resize 170x113 pic/gingerkitten-small.jpg
+
+# https://www.criver.com/sites/default/files/2017-10/129-Mouse_417x235.jpg
+convert pic/mouse-orig.jpg -trim -resize 208x118 pic/mouse-small.jpg
