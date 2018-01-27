4bbed76860fe360b8245ab67b3edeee5f55f8bbc
Justin Cinkelj <justin.cinkelj@xlab.si>
2018-01-27 14:40:27 +0100

    use pictures
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- kvader.py
+++ kvader.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
+# http://pyglet.readthedocs.io/en/pyglet-1.3-maintenance/programming_guide/quickstart.html
+# http://steveasleep.com/pyglettutorial.html
+
 import pyglet
 
 window = pyglet.window.Window()
+macka = pyglet.resource.image('pic/gingerkitten-small.jpg')
+mis = pyglet.resource.image('pic/mouse-small.jpg')
 label = pyglet.text.Label('Hello, world',
                           font_name='Times New Roman',
                           font_size=36,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
+
 @window.event
 def on_draw():
     window.clear()
+    macka.blit(0,0)
+    mis.blit(400, 300)
     label.draw()
 
 pyglet.app.run()
