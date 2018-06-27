f73dd328ab41973d2db6393dd028ce26cdc06f61
gregor <gregor>
2018-06-27 19:51:37 +0200

    dodal tractor-1 in prikolica sliko
    Signed-off-by: gregor <gregor>

--- gen-pic.sh
+++ gen-pic.sh
@@ -5,3 +5,7 @@ convert pic/gingerkitten-orig.jpg -trim -resize 170x113 pic/gingerkitten-small.j
 
 # https://www.criver.com/sites/default/files/2017-10/129-Mouse_417x235.jpg
 convert pic/mouse-orig.jpg -trim -resize 208x118 pic/mouse-small.jpg
+
+#convert pic/prikolica-1.png -trim +repage -fuzz 1% -transparent black pic/prikolica-1-small.png
+convert pic/prikolica-1.png -trim +repage pic/prikolica-1-small.png
+convert pic/tractor-1.png -trim +repage pic/tractor-1-small.png
